4bba1f8f7955442053549ac9a5a1bd5e8b733316
moli <moli@mili.com>
2020-10-10 15:04:48 +0800

    项目第一个文件

---
+++ data.php
@@ -0,0 +1,11 @@
+
+		<?php
+			$flag=$_GET['param'];
+			if($flag==1) {
+				echo '<div>苹果</div>';
+			}else if($flag==2) {
+				echo '<div>香蕉</div>';
+			}else {
+				echo '<div>橘子</div>';
+			}
+		?>
